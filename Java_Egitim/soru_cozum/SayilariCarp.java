55dd65e2cfcbcc0615f2e36d05cf6267c9799ac0
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2023-04-02 23:03:08 +0300

    Update SayilariCarp.java

--- Java_Egitim/soru_cozum/SayilariCarp.java
+++ Java_Egitim/soru_cozum/SayilariCarp.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class SayilariCarp {
     public static void main(String[] args) {
+        
+        System.out.println("*************************************************************");
+        System.out.println("           SAYILARI CARP UYGULAMASINA HOS GELDINIZ              ");
+        System.out.println("*************************************************************");
         Scanner in = new Scanner(System.in);
         int carpim = 1;
         while (true)
